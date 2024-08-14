cd6ea7865d4c053d4674daa1f13a11f5c02c752e
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2024-08-14 10:45:36 +0530

    create a business logic

--- backend/src/main/java/com/example/backend/model/User.java
+++ backend/src/main/java/com/example/backend/model/User.java
@@ -1,9 +1,15 @@
 package com.example.backend.model;
 
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     @Id
