064f942dbdfe50bafdff2771ec40a2982515e525
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2024-08-14 11:39:54 +0530

    Create HTTP Requests

--- backend/src/main/java/com/example/backend/service/UserService.java
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -14,7 +14,7 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> getAllUser() {
+    public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
