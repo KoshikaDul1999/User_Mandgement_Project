e9e968baa00573bcd670499bda95526ef14e4e85
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2024-08-20 16:18:20 +0530

    complete view users part

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { Link, useParams } from "react-router-dom";
+
 export default function Home() {
+
+    const [users, setUsers] = useState([]);
+
+    const {id} = useParams();
+
+    useEffect(() => {
+        loadUsers();
+    },[]);
+
+    const loadUsers = async () => {
+        const result = await axios.get("http://localhost:8080/api/users");
+        setUsers(result.data);
+    };
+
+    const deleteUser = async (id) => {
+        const result = await axios.delete(`http://localhost:8080/api/users/${id}`);
+        loadUsers();
+    }
+
     return (
        <div className="container">
             <div className="py-4">
@@ -14,14 +37,33 @@ export default function Home() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>user id</td>
-                            <td>user name</td>
-                            <td>user email</td>
-                            <td>user address</td>
-                            <td>user mobile</td>
-                            <td>edit , delete</td>
-                        </tr>
+                        {users.map((user,index) => (
+                            <tr>
+                                <th scope="row" key={index}>{index+1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.address}</td>
+                                <td>{user.mobile}</td>
+                                <td>
+                                    <Link className="btn btn-primary mx-2"
+                                        to={`/viewuser/${user.id}`}
+                                    >
+                                        View
+                                    </Link>
+                                    <Link className="btn btn-outline-primary mx-2"
+                                        to={`/edituser/${user.id}`}
+                                    >
+                                        Edit
+                                    </Link>
+
+                                    <button className="btn btn-danger mx-2"
+                                        onClick={() => deleteUser(user.id)}
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
--- frontend/src/users/AddUser.jsx
+++ frontend/src/users/AddUser.jsx
@@ -1,32 +1,56 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import axios from 'axios';
 
 export default function AddUser() {
+
+    let navigate = useNavigate()
+
+    const [user,setUser] = useState({
+        name:"",
+        email:"",
+        address:"",
+        mobile:"",
+    });
+
+    const {name, email, address, mobile} = user;
+
+    const onInputChange = (e) => {
+        setUser({...user, [e.target.name]: e.target.value});
+    };
+
+    const onSubmit=async (e) => {
+        e.preventDefault();
+        await axios.post("http://localhost:8080/api/users",user)
+        navigate("/")
+    };
+
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
                     <h2 className="text-center m-4">Register User</h2>
-                    <form>
+                    <form onSubmit={(e)=>onSubmit(e)}>
                         <div className="mb-3">
-                            <label>
+                            <label htmlFor="Name" className="form-label">
                                 Name
                             </label>
-                            <input type="text" className="form-control" placeholder="enter your name" />
+                            <input type="text" className="form-control" placeholder="enter your name" value={name} onChange={(e)=>onInputChange} />
                         </div>
                         <div className="mb-3">
-                            <label>
+                            <label htmlFor="Email" className="form-label">
                                 E-mail
                             </label>
                             <input type="text" className="form-control" placeholder="enter your email" />
                         </div>
                         <div className="mb-3">
-                            <label>
+                            <label htmlFor="Address" className="form-label">
                                 Address
                             </label>
                             <input type="text" className="form-control" placeholder="enter your address" />
                         </div>
                         <div className="mb-3">
-                            <label>
+                            <label htmlFor="Mobile" className="form-label" >
                                 Contact Number
                             </label>
                             <input type="text" className="form-control" placeholder="enter your mobile number" />
