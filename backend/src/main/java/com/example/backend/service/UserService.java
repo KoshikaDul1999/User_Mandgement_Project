9478cfc3a282b63ba6885fc947689036c7d92c52
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2024-08-21 10:14:43 +0530

    Update UserService.java

--- backend/src/main/java/com/example/backend/service/UserService.java
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -35,21 +35,6 @@ public class UserService {
         return userRepository.save(user);
     }
 
-    /*
-     public User updateUser(String id, User userDetails) {
-        // Check if the user exists
-        return userRepository.findById(id)
-                .map(user -> {
-                    user.setName(userDetails.getName());
-                    user.setEmail(userDetails.getEmail());
-                    user.setAddress(userDetails.getAddress());
-                    user.setMobile(userDetails.getMobile());  // Assuming mobile field exists in your User model
-                    return userRepository.save(user);
-                })
-                .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
-    }
-     */
-
     public void deleteUser (String id) {
         userRepository.deleteById(id);
     }
